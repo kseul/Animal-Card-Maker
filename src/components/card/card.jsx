16e230c0f053bc064aadb89a35b7bc23c002c312
seul <kseul06@gmail.com>
2022-08-06 22:21:54 +0900

    ADD : Card and Edit form component
    - Card Edit UI 작성
    - button 컴포넌트 생성
    - maker.js State 초기값 수정 및 카드 추가

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -4,7 +4,7 @@ import styles from './card.module.css';
 const DEFAULT_IMAGE = '/images/default_logo.png';
 
 const Card = ({ card }) => {
-  const { name, age, color, title, hobby, message, theme, fileName, fileURL } =
+  const { name, age, color, type, hobby, message, theme, fileName, fileURL } =
     card;
 
   const url = fileURL || DEFAULT_IMAGE;
@@ -14,11 +14,20 @@ const Card = ({ card }) => {
       <img className={styles.avatar} src={url} alt='profile photo' />
       <div className={styles.info}>
         <h1 className={styles.name}>{name}</h1>
-        <p className={styles.title}>{title}</p>
-        <p className={styles.age}>{age}</p>
-        <p className={styles.color}>{color}</p>
-        <p className={styles.hobby}>{hobby}</p>
-        <p className={styles.message}>{message}</p>
+        <p className={styles.type}>{type}</p>
+        <p className={styles.age}>
+          <span>나이: </span>
+          {age}
+        </p>
+        <p className={styles.color}>
+          <span>털색: </span>
+          {color}
+        </p>
+        <p className={styles.hobby}>
+          <span>취미&특기: </span>
+          {hobby}
+        </p>
+        <p className={styles.message}>"{message}"</p>
       </div>
     </li>
   );
--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,5 +1,66 @@
 import React from 'react';
 import styles from './card_edit_form.module.css';
+import Button from '../button/button';
+import ImageFileInput from '../../image_file_input/image_file_input';
 
-const CardEditForm = ({ card }) => <h1>{card.id}</h1>;
+const CardEditForm = ({ card }) => {
+  const { name, age, color, type, hobby, message, theme, fileName, fileURL } =
+    card;
+
+  const onSubmit = () => {};
+  return (
+    <form className={styles.form}>
+      <input
+        className={styles.input}
+        type='text'
+        name='name'
+        defaultValue={name}
+      />
+      <input
+        className={styles.input}
+        type='text'
+        name='type'
+        defaultValue={type}
+      />
+      <select className={styles.select} name='age' defaultValue={age}>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option>
+        <option value='6'>6</option>
+        <option value='7'>7</option>
+        <option value='8'>8</option>
+        <option value='9'>9</option>
+        <option value='10'>10</option>
+        <option value='11'>11</option>
+        <option value='12'>12</option>
+        <option value='13'>13</option>
+        <option value='14'>14</option>
+        <option value='15'>15</option>
+      </select>
+      <input
+        className={styles.input}
+        type='text'
+        name='color'
+        defaultValue={color}
+      />
+      <input
+        className={styles.input}
+        type='text'
+        name='hobby'
+        defaultValue={hobby}
+      />
+      <textarea
+        className={styles.textarea}
+        name='message'
+        defaultValue={message}
+      />
+      <div className={styles.fileInput}>
+        <ImageFileInput />
+      </div>
+      <Button name='Delete' onClick={onSubmit} />
+    </form>
+  );
+};
 export default CardEditForm;
