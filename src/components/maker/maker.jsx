d79ee368d40f036ad8c2c96ceaa1f57d2e0ebe65
seul <kseul06@gmail.com>
2022-08-06 17:50:57 +0900

    ADD : card and card_edit_form component
    - card & card_edit_form 컴포넌트 생성
    - maker.jsx State 초기설정 및 props 전달
    - editor & preview 컴포넌트에서 map 확인

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Editor from '../editor/editor';
 import Footer from '../footer/footer';
@@ -7,6 +7,41 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Hamong',
+      age: '1',
+      color: 'white',
+      title: '댕댕이',
+      hobby: '집 엉망으로 만들기',
+      message: '다 뿌셔버리게써',
+      fileName: 'hamong',
+      fileURL: 'hamong.png',
+    },
+    {
+      id: '2',
+      name: 'Berry',
+      age: '5',
+      color: 'brown',
+      title: '댕댕이',
+      hobby: '인형 망가뜨리기',
+      message: '노는게 젤조앙',
+      fileName: 'berry',
+      fileURL: 'berry.png',
+    },
+    {
+      id: '3',
+      name: 'Nalsoon',
+      age: '6',
+      color: 'black',
+      title: '댕댕이',
+      hobby: '산책하기',
+      message: '놀아줘 주인!',
+      fileName: 'nalsoon',
+      fileURL: 'nalsoon.png',
+    },
+  ]);
   const navigate = useNavigate();
   const onLogout = () => {
     authService.logout();
@@ -24,8 +59,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
